e61d096c3fca0c325cb278c9195e0040876e1f90
Shubham <bizmailsam@gmail.com>
2019-04-23 09:50:00 +0530

    added a side-drawer

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -6,10 +6,27 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("List App"),
+      appBar: AppBar(
+        title: Text("List App"),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text("Choose"),
+            ),
+            ListTile(
+              title: Text("Something Something"),
+              onTap: () {},
+            )
+          ],
         ),
-        body: ProductsManager({"title": "Icecream Please 😍😍😍", "imageURL": "lib/assets/cover.jpg"}),
-      );
+      ),
+      body: ProductsManager({
+        "title": "Icecream Please 😍😍😍",
+        "imageURL": "lib/assets/cover.jpg"
+      }),
+    );
   }
 }
