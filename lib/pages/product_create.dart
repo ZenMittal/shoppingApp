221875f86d905877159c7cbea0cea50666654cc4
Shubham <bizmailsam@gmail.com>
2019-05-09 12:09:22 +0530

    reduced build function of product_create

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,51 +16,67 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _descrValue = "";
   double _priceValue = 0.0;
 
+  Widget _buildEmailInput() {
+    return TextField(
+      onChanged: (String value) {
+        setState(() {
+          _titleValue = value;
+        });
+      },
+      decoration: InputDecoration(labelText: "Product Name"),
+    );
+  }
+
+  Widget _buildDescrInput() {
+    return TextField(
+      maxLines: 4,
+      onChanged: (String value) {
+        setState(() {
+          _descrValue = value;
+        });
+      },
+      decoration: InputDecoration(labelText: "Product Desciption"),
+    );
+  }
+
+  Widget _buildPriceInput() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      onChanged: (String value) {
+        setState(() {
+          _priceValue = double.parse(value);
+        });
+      },
+      decoration: InputDecoration(labelText: "Product Price"),
+    );
+  }
+
+  void _submitForm() {
+    widget.addProd({
+      "title": _titleValue,
+      "description": _descrValue,
+      "price": _priceValue,
+      "imageURL": "lib/assets/img/cover.jpg"
+    });
+    Navigator.pushReplacementNamed(context, "/products");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: ListView(
         children: <Widget>[
-          TextField(
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
-            decoration: InputDecoration(labelText: "Product Name"),
+          _buildEmailInput(),
+          _buildDescrInput(),
+          _buildPriceInput(),
+          SizedBox(
+            height: 10.0,
           ),
-          TextField(
-            maxLines: 4,
-            onChanged: (String value) {
-              setState(() {
-                _descrValue = value;
-              });
-            },
-            decoration: InputDecoration(labelText: "Product Desciption"),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            onChanged: (String value) {
-              setState(() {
-                _priceValue = double.parse(value);
-              });
-            },
-            decoration: InputDecoration(labelText: "Product Price"),
-          ),
-          SizedBox(height: 10.0,),
           RaisedButton(
             child: Text("Save Product"),
             color: Theme.of(context).accentColor,
-            onPressed: () {
-              widget.addProd({
-                "title": _titleValue,
-                "description": _descrValue,
-                "price": _priceValue,
-                "imageURL": "lib/assets/img/cover.jpg"
-              });
-              Navigator.pushReplacementNamed(context, "/products");
-            },
+            onPressed: _submitForm,
           ),
         ],
       ),
